4148ebee8996802d8912b50b065f1aa2ca13165c
Fabiola <barcelosfabiola@gmail.com>
2022-02-08 22:54:37 -0300

    exercícios variáveis

--- modulo1/variaveis/template_javascript/index.js
+++ modulo1/variaveis/template_javascript/index.js
@@ -1,1 +1,28 @@
+/*let nome
+let idade
+console.log(typeof nome, idade)*/
+//O valor retornado foi undefined porque não foram atribuídos valores às variáveis.*/
+
+/*let nome = prompt("Qual o seu nome?")
+let idade = prompt ("Qual a sua idade?")
+console.log(typeof nome, typeof idade)
+//O resultado é string, o output do comando prompt sempre é string.
+
+console.log("Olá,", nome, "você tem", idade, "anos.")*/
+
+/*exercício 2
+let gato = prompt("Você tem gato?")
+let cachorro = prompt ("Você tem cachorro?")
+let pet = prompt("Aceitas um pet?")
+console.log("Você tem gato?", gato, "Você tem cachorro?", cachorro, "Aceitas um pet?", pet)
+*/
+
+/*exercício 3
+let a = 10
+let b = 25
+let c
+ c = a
+ a = b
+ b = c
+ console.log(a, b)*/
 
