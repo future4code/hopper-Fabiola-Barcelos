a106ab3b504c121340a7272005f61411c8e9ba46
fab-fabs <fab-fabs@github.com>
2022-02-17 18:49:48 -0300

    exercícios de interpretação de código string e arrays

---
+++ modulo1/string-arrays/template_javascript/index.js
@@ -0,0 +1,17 @@
+// 1
+//a. undefined
+// b. null
+// c. 11
+// d. 3
+// e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12 ,13]
+// f. 9
+//2
+//SUBI NUM ÔNIBUS EM MIRROCOS, 27
+
+// let emailDoUsuario = prompt("Qual o seu e-mail?")
+// let nomeDoUsuario = prompt("Qual o seu nome?")
+// console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso.
+// Seja bem-vinda, ${nomeDoUsuario}!`)
+
+
+
