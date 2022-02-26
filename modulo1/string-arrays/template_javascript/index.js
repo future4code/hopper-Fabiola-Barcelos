d8c9ed2871b2cf9b62552e7b919808aa5759058c
fab-fabs <fab-fabs@github.com>
2022-02-26 00:24:35 -0300

    alguns exercícios

--- modulo1/string-arrays/template_javascript/index.js
+++ modulo1/string-arrays/template_javascript/index.js
@@ -1,11 +1,12 @@
-// 1
+// EXERCÍCIOS DE INTERPRETAÇÃO
+//1
 //a. undefined
 // b. null
 // c. 11
 // d. 3
 // e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12 ,13]
 // f. 9
-//2
+//2.
 //SUBI NUM ÔNIBUS EM MIRROCOS, 27
 
 // let emailDoUsuario = prompt("Qual o seu e-mail?")
@@ -13,5 +14,10 @@
 // console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso.
 // Seja bem-vinda, ${nomeDoUsuario}!`)
 
-
-
+//EXERCÍCIO DE ESCRITA DE CÓDIGO
+/*1
+const nomeDoUsuario = prompt("Qual o seu nome?")
+const emailDoUsuario = prompt("Qual seu e-mail?")
+console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}!`)
+*/
+//*/2
