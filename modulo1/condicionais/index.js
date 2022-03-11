cf763a47f21dbfaff7d2039d979437e190486b30
fab-fabs <fab-fabs@github.com>
2022-03-10 23:41:48 -0300

    exercícios condicionais, leitura finalizado; escrita em andamento

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,43 @@
+//INTERPRETAÇÃO
+//1.
+//a) O código solicita um número para o usuário; a função divide o número por 2 e verifica se o resto é igual a zero.
+//b) Passou no teste será impresso caso o número inserido seja par.
+//c) Não passou no teste será impresso caso o número seja ímpar
+
+//2.
+//a) O código é uma lista de preço de frutas que solicita ao usuário o que ele deseja e devolve o preço do item.
+//b) O preço da fruta, maçã, é R$ 2.25
+//c) se tirar o break, o código não encontra a fruta Pêra e o que é impresso é o default.
+
+//3. a) a primeira linha solicita um número ao usuário, como prompt recebe string, a função number transforma a resposta em número.
+//b) 10: mensagem no terminal : Esse número passou no teste;
+//-10, não aparecerá nada
+//c) haverá erro pq a variável mensagem está dentro do bloco da condicional, não pode ser acessada pelo escopo global pq nhm especificou isso.
+
+//ESCRITA DE CÓDIGO
+//1. 
+/*let idadeUsuario = Number(prompt("Qual a sua idade?"))
+if (idadeUsuario > 18) {
+    console.log("Você pode dirigir")
+       
+}else if (idadeUsuario<18) {
+    console.log("Você não pode dirigir")
+  
+}
+*/
+
+
+/*2
+let turnoAluno = prompt("Qual o turno que você estuda? Digite M(matutino), V(verpertino) ou N(noturno).")
+
+if (turnoAluno === "M" || turnoAluno === "m") {
+    console.log("Bom dia!")
+   } else if (turnoAluno === "V" || turnoAluno === "v"){
+console.log("Boa tarde!")
+    } else if (turnoAluno === "N" || turnoAluno === "n"){
+console.log("Boa noite!")
+    } else {
+console.log("Erro! Digite M, V ou N.")
+    }
+*/
+
