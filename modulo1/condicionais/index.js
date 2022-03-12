c122993ef56f277d3feefb07978602f79c07f0c9
fab-fabs <fab-fabs@github.com>
2022-03-11 23:49:31 -0300

    exercícios condicioanis

--- modulo1/condicionais/index.js
+++ modulo1/condicionais/index.js
@@ -41,3 +41,133 @@ console.log("Erro! Digite M, V ou N.")
     }
 */
 
+/*3
+let turnoAluno = prompt("Qual o turno que você estuda? Digite M(matutino), V(verpertino) ou N(noturno).")
+turnoAluno.toUpperCase()
+switch (turnoAluno.toUpperCase()) {
+    case 'M':
+        console.log('Bom dia!')
+        break;
+    case 'V':
+        console.log('Boa tarde!')
+        break;
+    case 'N':
+        console.log('Boa noite!')
+        break; 
+
+    default:
+        console.log('Erro! Por favor, digite M, V ou N.')
+        break
+}
+*/
+
+/*4
+//fantasia e ingresso<15
+//perguntar qual o gênero do filme, perguntart valor do ingresso
+
+let generoFilme = prompt("Qual gênero de filme você vai assistir?")
+let valorIngresso = Number(prompt(`Qual o valor do ingresso?`))
+if (generoFilme.toLowerCase() === `fantasia` && valorIngresso <= 15) {
+    console.log(`Bom filme!`)
+}else {
+    console.log(`Escolha outro filme :(`)
+} 
+*/
+
+//DESAFIO
+/*1.
+let generoFilme = prompt("Qual gênero de filme você vai assistir?")
+let valorIngresso = Number(prompt(`Qual o valor do ingresso?`))
+
+if (generoFilme.toLowerCase() === `fantasia` && valorIngresso <= 15) {
+    let lanche = prompt(`Qual snack você vai querer?`)    
+    console.log(`Bom filme!`)
+    console.log(`Aproveite seu ${lanche}!`) 
+}else {
+    console.log(`Escolha outro filme :(`)
+} 
+*/
+
+
+
+//2. 
+let nomeUsuario = prompt(`Insira seu nome completo`)
+let tipoJogo = prompt(`Digite IN para jogo internacional ou DO para jogo doméstico`)
+let etapaJogo = prompt(`Semi-final, decisão do terceiro lugar ou final?`)
+let categoria = prompt(`Escolha a categoria: 1, 2, 3 ou 4`)
+let quantidadeIngressos = Number(prompt(`Quantos ingressos você deseja?`))
+console.log(`Dados da compra:`)
+console.log(`Nome do cliente: ${nomeUsuario}`)
+
+if (tipoJogo.toLowerCase() === `internacional`) {
+    console.log(`Tipo de jogo: internacional`)
+} else if (tipoJogo.toLocaleLowerCase() ===  `doméstico`)  {
+    console.log(`Tipo de jogo: nacional`)
+} else {
+    console.log(`Erro! Escolha o tipo de jogo corretamente`)
+   } 
+
+   switch (etapaJogo.toLowerCase()) {
+       case `semi-final`:
+           console.log(`Etapa do jogo: semi-final`)
+           break;
+        case `final`:
+            console.log(`Etapa do jogo: final`)
+            break;
+        case `terceiro lugar`:
+            console.log( `Etapa do jogo: disputa pelo terceiro lugar`)
+            break;
+       default:
+           console.log(`Erro! Insira a etapa do jogo: final, semi-final ou disputa pelo terceiro lugar.`)
+           break;
+   }
+
+    switch (categoria) {
+        case `1`:
+            console.log(`Categoria: 1`)
+            break;
+        case `2`:
+            console.log(`Categoria: 2`)
+            break; 
+        case `3`:
+            console.log(`Categoria: 3`)
+            break;
+        case `4`:
+            console.log(`Categoria: 4`)
+            break;   
+    
+        default:
+            console.log(`Erro! Insira a categoria corretamente.`)
+            break;
+    }
+
+    console.log(`Quantidade de ingressos: ${quantidadeIngressos}`)
+    console.log(`Valores:`)
+/*let sf1 =  
+
+1320
+let sf2 = 880
+let sf3 = 550
+let sf4 = 220
+let dt1 = 660
+let dt2 = 440
+let dt3 = 330
+let dt4 = 170
+let fi1 = 1980
+let fi2 = 1320
+let fi3 = 880
+let fi4 = 330*/
+
+
+if (tipoJogo === "doméstico" && etapaJogo === `disputa do terceiro lugar` && categoria === "1") {
+    console.log(`Valor do ingresso: R$ 660,00`)
+} else if (tipoJogo === "doméstico" && etapaJogo === `disputa do terceiro lugar` && categoria === "2"){
+    console.log(`Valor do ingresso: R$ 440,00`)
+} else if (tipoJogo === "doméstico" && etapaJogo === `disputa do terceiro lugar` && categoria === "3"){
+    console.log(`Valor do ingresso: R$ 330,00`)
+} else if (tipoJogo === "doméstico" && etapaJogo === `disputa do terceiro lugar` && categoria === "4"){
+    console.log("Valor do ingresso: R$170,00")
+} else{
+    console.log(`Erro! Confira as informações.`)
+}
+
