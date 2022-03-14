79fbbf53785d391b982a4d87d876bf1b7b383adb
fab-fabs <fab-fabs@github.com>
2022-03-13 23:41:22 -0300

    exercícios leitura laços

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,67 @@
+//INTERPRETAÇÃO
+/*1. o código repete a ação até o índice 5 da array
+a partir do índice 0, até o índice 5, somar o conteúdo da posição ao valor, que inicia no zero.
+
+2. imprime os número maiores que 18
+console log = 19, 21, 23, 25, 27, 30
+
+3.
+const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+let quantidadeAtual = 0
+while(quantidadeAtual < quantidadeTotal){
+  let linha = ""
+  for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+    linha += "*"
+  }
+  console.log(linha)
+  quantidadeAtual++
+}
+
+
+*
+**
+***
+****
+a cada linha, adiciona um *
+adiciona 1 na quantidade atual
+quantidade total = 4
+0<4
+1<4
+2<4
+3<4
+4
+*/
+
+
+
+
+//ESCRITA
+//1.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
