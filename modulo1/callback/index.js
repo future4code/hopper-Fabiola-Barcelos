cb7b1c2f20c4e353b9033649c6385a59d0634df9
fab-fabs <fab-fabs@github.com>
2022-03-15 22:09:48 -0300

    arquivos callback

---
+++ modulo1/callback/index.js
@@ -0,0 +1,29 @@
+
+const usuarios = [
+    { nome: "Amanda Rangel", apelido: "Mandi" },
+    { nome: "Laís Petra", apelido: "Laura" },
+    { nome: "Letícia Chijo", apelido: "Chijo" }
+  ]
+  
+  const novoArrayA = usuarios.map((item, index, array) => {
+     console.log(item, index, array)
+  })
+
+
+  const pokemons = [
+    { nome: "Bulbasaur", tipo: "grama" },
+    { nome: "Bellsprout", tipo: "grama" },
+    { nome: "Charmander", tipo: "fogo" },
+    { nome: "Vulpix", tipo: "fogo" },
+    { nome: "Squirtle", tipo: "água" },
+    { nome: "Psyduck", tipo: "água" },
+ ]
+
+ console.log(pokemons)
+
+ function retornarPokemon (pokemon, indice){
+     console.log(`O nome do pokemon ${pokemon.nome} é  e sua posição é ${indice}`)
+ }
+ const nomesPokemons = pokemons.map(retornarPokemon)
+ console.log(nomesPokemons)
+ 
